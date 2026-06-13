1b85af202407df828551e0590fed232a93ae84a9
profamine <amahanamine0@gmail.com>
2026-06-13 15:56:09 +0100

    Android buil

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.moroccosmartguide.app',
+  appName: 'Morocco Smart Guide',
+  webDir: 'dist'
+};
+
+export default config;
